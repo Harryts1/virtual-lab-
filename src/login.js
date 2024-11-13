3e5f1959afa41bd919fedecfbc7aaca24dca6083
Harryts1 <harrytrumansuhalim8@gmail.com>
2024-11-13 22:46:00 +0700

    link

--- src/login.js
+++ src/login.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const password = document.getElementById('password').value;
             
             try {
-                const response = await fetch('http://localhost:5000/api/login', {
+                const response = await fetch('https://virtual-lab-beige.vercel.app/api/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
